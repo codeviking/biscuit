f38dcc16614429e8b74c833e772b2efb0491b23f
Sam Skjonsberg <skoneberg@gmail.com>
2014-09-12 11:38:34 -0700

    Small changes to biscuit.bake; Add auto-prefixer and cache breaking.

--- biscuit.js
+++ biscuit.js
@@ -5,6 +5,8 @@ var path = require('path');
 var rimraf = require('rimraf');
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
+var cachebreaker = require('gulp-cache-breaker');
+var autoprefixer = require('gulp-autoprefixer');
 var less = require('gulp-less');
 
 function Biscuit(src) {
@@ -21,25 +23,33 @@ Biscuit.prototype.bake = function(dir) {
   if(!fs.existsSync(dir) || !fs.statSync(dir).isDirectory()) {
     fs.mkdirSync(dir);
   }
-  this.clean(dir);
-  return q.all(
-    this.html(dir),
-    this.less(dir)
-  );
+  // TODO: should this be stream instead of promise based?
+  return this.clean(dir)
+      .then(this.less.bind(this, dir))
+      .then(this.html.bind(this, dir));
 };
 
 Biscuit.prototype.clean = function(dir) {
-  rimraf.sync(path.resolve(dir, '*'));
-  return this;
+  var cleaned = q.defer();
+  // TODO: should we use gulp-rimraf instead?
+  rimraf(path.resolve(dir, '*'), function(err) {
+    if(err) {
+      cleaned.reject();
+    } else {
+      cleaned.resolve();
+    }
+  });
+  return cleaned.promise;
 };
 
 Biscuit.prototype.html = function(dir) {
   var copied = q.defer();
   gulp.src(path.resolve(this.src , '**/*.html'))
     .pipe(plumber(copied.reject))
+    .pipe(cachebreaker(dir))
     .pipe(gulp.dest(dir))
     .on('end', copied.resolve);
-  return copied;
+  return copied.promise;
 };
 
 Biscuit.prototype.less = function(dir) {
@@ -48,9 +58,10 @@ Biscuit.prototype.less = function(dir) {
   gulp.src(path.resolve(this.src, 'styles.less'))
     .pipe(plumber(compiled.reject))
     .pipe(less({ compress: true }))
+    .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest(dir))
     .on('end', compiled.resolve);
-  return compiled;
+  return compiled.promise;
 };
 
 module.exports = Biscuit;
