9955d123c28f32691eb8ff2c23f0d3cc2e414ba8
Sam Skjonsberg <skoneberg@gmail.com>
2014-10-02 20:53:25 -0700

    Add a simple dummy test to make sure things are sane.

---
+++ tests/flapjack_test.js
@@ -0,0 +1,10 @@
+var Flapjack = require('../flapjack');
+var assert = require('assert');
+
+describe('Flapjack', function() {
+  // This is a simple, dummy test.
+  // TODO: Actual test coverage...
+  it('is a function', function() {
+    assert(typeof Flapjack === 'function');
+  });
+});
